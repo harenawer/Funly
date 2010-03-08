7d9d67eee2402d74cdb87b474c968267f5c9272b
Sergio <sergio@corum.(none)>
2010-03-08 23:12:34 +0100

    Adding files and layout

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='funly',
+      version='0.1.0',
+      author='Sergio Pascual',
+      author_email='sergiopr@astrax.fis.ucm.es',
+      license='GPLv3',
+      package_dir={'': 'src'},
+      py_modules=['funly']
+      )
